21b3d5c300ba65e94b499b8bc4fc79bdebaee60a
hema <bhemavallika@gmail.com>
2025-06-25 20:13:00 +0530

    file handling in this

--- exceptionHandling.py
+++ exceptionHandling.py
@@ -16,13 +16,24 @@
 # print ("end i have done this")
 
 
-age = int(input("tell your age: "))
-try:
-    if age<10 or age >18:
-        raise ValueError("Your age must be b'w 10 and 18")
-    else:
-        print("welcome to the party")
-except Exception as err:
-    print("your error is",err)    
-print("the club will start soon")    
+# age = int(input("tell your age: "))
+# try:
+#     if age<10 or age >18:
+#         raise ValueError("Your age must be b'w 10 and 18")
+#     else:
+#         print("welcome to the party")
+# except Exception as err:
+#     print("your error is",err)    
+# print("the club will start soon")    
 
+
+
+
+# file handling
+# ~~~~~~~~~~~~~~~~~~
+# p=open('main.py')
+# print(p.read())
+
+# r= open('superman.txt','a')
+# r.write("I am appending something to this file")
+# r.close()
