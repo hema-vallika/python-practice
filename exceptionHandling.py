888a67a6f0a8a3300e2e01926d245e8c4355729a
hema <bhemavallika@gmail.com>
2025-06-25 20:05:44 +0530

    exception handling

---
+++ exceptionHandling.py
@@ -0,0 +1,28 @@
+# a =  int(input("Enter a number: "))
+
+# try:
+#     print(10/a)
+
+# except ZeroDivisionError:
+#     print("You have entered zero. Ypu can't divide by zero")
+
+# except Exception as err:
+#     print("You have entered a wrong input", err)  
+# else: #try nhi chalega toh ye chalega
+#     print("good there is no exception")      
+# finally:
+#     print("I will always execute")
+    
+# print ("end i have done this")
+
+
+age = int(input("tell your age: "))
+try:
+    if age<10 or age >18:
+        raise ValueError("Your age must be b'w 10 and 18")
+    else:
+        print("welcome to the party")
+except Exception as err:
+    print("your error is",err)    
+print("the club will start soon")    
+
